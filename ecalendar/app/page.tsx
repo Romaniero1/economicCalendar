81f4e1d24b057f5527eb8911bbc71ebca6c8e458
Romaniero1 <romaniero1@gmail.com>
2023-09-11 19:34:57 +0300

    bg fix

--- ecalendar/app/page.tsx
+++ ecalendar/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 				<meta name='description' content='Vittaverse' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main>
+			<main className='bg-black'>
 				<Calendar />
 			</main>
 		</div>
